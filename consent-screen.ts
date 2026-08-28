ad3ca7d1af75a2590c6d9cfc5352f479842b0e7d
daviss73-sudo <daviss73@matc.edu>
2026-08-28 10:04:57 -0500

    Delete consent-screen.ts

--- consent-screen.ts
+++
@@ -1,5 +0,0 @@
-import type { ConsentPreferences } from '../types/consent.ts';
-
-export function showConsentScreen(): Promise<ConsentPreferences> {
-  return Promise.resolve({} as ConsentPreferences);
-}
